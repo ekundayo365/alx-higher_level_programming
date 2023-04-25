e1ee9bce68336f369f4b4f3907b63437a7be708b
ekundayo365 <damola.ekundayo@gmail.com>
2023-04-25 11:36:50 +0100

    Newest

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST req to URL $!, display response body
-curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"
+# Bash script that sends a JSON POST request
+curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
